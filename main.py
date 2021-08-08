5b430682caf3e2682599a87da4ca92bff0ba5dac
lej4545 <lej4545@naver.com>
2021-08-09 04:28:05 +0900

    캐릭터 margin 고려, 난이도 증가 설정

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+
 import pygame
 import random
 import math
+
+
+def set_difficulty(poo_list, min_speed, max_speed, max_num, max_x_position):
+    # 똥 내려오는 좌표 설정, 똥 개수 설정
+    while True:
+        randomNumber = random.randrange(0, max_x_position)
+        if len(poo_list) < max_num:
+            if poo_list.count(randomNumber) == 0:
+                temp_dict = {}
+                temp_dict["poo"] = pygame.image.load("ddong.png")
+                temp_dict["x"] = randomNumber
+                temp_dict["y"] = 0
+                temp_dict["speed"] = random.uniform(min_speed, max_speed)
+                poo_list.append(temp_dict)
+        else:
+            break
+
 
 def start_game():
     # 전연변수 설정: 게임 스크린 사이즈 및 초기 설정
@@ -8,10 +26,10 @@ def start_game():
     background = pygame.image.load("background.jpg")
     screen_width, screen_height = background.get_rect().size
     screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("똥피하기")
+    pygame.display.set_caption("똥 피하기")
 
     # 폰트 및 색상 정의
-    game_font = pygame.font.Font(None, 40) # 폰트, 크기
+    game_font = pygame.font.Font(None, 40)  # 폰트, 크기
     large_font = pygame.font.SysFont(None, 80)
     small_font = pygame.font.Font(None, 32)
     large_font_size = 80
@@ -27,35 +45,27 @@ def start_game():
 
     # 똥의 좌표값 설정
     poo_ylist = []
-    poo = []
-    for i in range(0, 10):
-        poo.append(pygame.image.load("ddong.png"))
+    poo = [pygame.image.load("ddong.png")]
     poo_size = poo[0].get_rect().size
-    # print(poo_size)
     poo_width = poo_size[0]
     poo_height = poo_size[1]
     poo_xpos = random.randrange(0, screen_width - poo_width)
     poo_ypos = 0
+
+    poo_list = []
 
     mouse_xpos = 0
     mouse_ypos = 0
     x_touch = False
     y_touch = False
 
-    poo_list = []
 
-    # 똥 내려오는 초기 좌표 설정, 똥은 최대 10개로 설정
-    while True:
-        randomNumber = random.randrange(0, screen_width - poo_width)
-        if poo_list.count(randomNumber) == 0:
-            temp_dict = {}
-            temp_dict["poo"] = pygame.image.load("ddong.png")
-            temp_dict["x"] = randomNumber
-            temp_dict["y"] = 0
-            temp_dict["speed"] = random.uniform(0.7, 1.3)
-            poo_list.append(temp_dict)
-        if len(poo_list) == 10:
-            break
+    init_min_speed = 0.7
+    init_max_speed = 1.2
+    init_max_num = 7
+
+    max_x_position = screen_width - poo_width
+    set_difficulty(poo_list, init_min_speed, init_max_speed, init_max_num, max_x_position)
 
     game_over = False
 
@@ -64,14 +74,29 @@ def start_game():
     # 다시 말해, 똥과 캐릭터가 겹치지 않을 때까지 게임 실행. 겹치면 게임 종료
 
     while True:
+
         timer = math.floor(pygame.time.get_ticks()/1000)
         score = math.floor(timer)
+        min_speed = score * 0.02 + init_min_speed
+        max_speed = score * 0.02 + init_max_speed
+        max_num = score * 0.1\
+                  + init_max_num
+        if len(poo_list) < max_num:
+            randomNumber = random.randrange(0, max_x_position)
+            if poo_list.count(randomNumber) == 0:
+                temp_dict = {}
+                temp_dict["poo"] = pygame.image.load("ddong.png")
+                temp_dict["x"] = randomNumber
+                temp_dict["y"] = 0
+                temp_dict["speed"] = random.uniform(min_speed, max_speed)
+                poo_list.append(temp_dict)
+
         for poo in poo_list:
             poo["y"] += poo["speed"]
             if poo["y"] + poo_height >= screen_height:
                 poo["x"] = random.randrange(0, screen_width - poo_width)
                 poo["y"] = 0
-                poo["speed"] = random.uniform(0.7, 1.3)
+                poo["speed"] = random.uniform(min_speed, max_speed)
 
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEMOTION:
@@ -79,16 +104,17 @@ def start_game():
                     mouse_ypos = event.__dict__["pos"][1]
                     character_xpos = mouse_xpos - character_width / 2
                     character_ypos = mouse_ypos - character_height / 2
-            if character_xpos <= poo["x"] <= character_xpos + character_width:
+                    character_margin = 18
+            if character_xpos <= poo["x"] <= character_xpos + character_width - character_margin:
                 x_touch = True
 
-            if character_xpos <= poo["x"] + poo_width <= character_xpos + character_width:
+            if character_xpos + character_margin <= poo["x"] + poo_width <= character_xpos + character_width:
                 x_touch = True
 
-            if character_ypos <= poo["y"] <= character_ypos + character_height:
+            if character_ypos <= poo["y"] <= character_ypos + character_height - character_margin:
                 y_touch = True
 
-            if character_ypos <= poo["y"] + poo_height <= character_ypos + character_height:
+            if character_ypos + character_margin <= poo["y"] + poo_height <= character_ypos + character_height:
                 y_touch = True
 
             if x_touch is True and y_touch is True:
